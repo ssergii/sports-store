811513664937c9a8310f3f1b48b339374be5f0ae
Sergii Safatiuk <s.safatiuk@gmail.com>
2018-02-27 21:52:31 +0100

    Config ninject and add binding between IProductRepository with Product list.

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,47 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+using Moq;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            var products = new List<Product>()
+            {
+                new Product { Name = "Piłka nożna", Price = 25 },
+                new Product { Name = "Deska surfingowa", Price = 179 },
+                new Product { Name = "Buty do biegania", Price = 95 }
+            };
+
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(o => o.Products).Returns(products);
+
+            _kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+    }
+}
